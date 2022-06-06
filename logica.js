44b74895f367249327e90acaa4eb8199ad861fbb
pachperdev <pachperdev@gmail.com>
2022-06-06 17:11:32 -0500

    add back

--- logica.js
+++ logica.js
@@ -1,1 +1,19 @@
 
+const mysql = require("mysql");
+var con = mysql.createConnection({
+
+    host: "telematica3229.ci94bml2ssya.us-east-2.rds.amazonaws.com",
+    user: "admin",
+    password: "Fosakia132balon",
+    database: "bdtelematica"
+});
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Conectado")
+
+})
+con.query('select * from arbitros order by id', (err, mess, fields) => {
+
+    console.log(mess)
+
+});
