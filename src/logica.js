e44430ae434950a01794ca70544e3d87084ab2d2
mafeAS <101601896+mafeAS@users.noreply.github.com>
2022-06-06 19:57:36 -0500

    version 1

---
+++ src/logica.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const mysql = require("mysql");
+const path = require("path");
+
+
+var con = mysql.createConnection({
+
+    host: "telematica3229.ci94bml2ssya.us-east-2.rds.amazonaws.com",
+    user: "admin",
+    password: "Fosakia132balon",
+    database: "bdtelematica"
+});
+
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Conectado")
+
+})
+
+app.get('/data' ,(req,res)=>{
+
+    con.query('select nombre from equipos order by id', (err, mess, fields) => {
+
+        res.status(200).json({
+        data:mess,
+        
+
+        });
+        console.log(mess)
+    
+    });
+
+
+});
+
+
+//settings
+app.set('port', process.env.PORT || 3000)
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: false })); 
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.listen(app.get('port'), ()=>{
+    console.log('server on port 3000')
+});
---
+++ src/public/js/index.js
@@ -0,0 +1,29 @@
+const partido1 = document.getElementById("partidovalue")
+const partido2 = document.getElementById("partidovalue2")
+const partido3 = document.getElementById("partidovalue3")
+const partido4 = document.getElementById("partidovalue4")
+function partido(){
+  fetch('data')
+  .then(response =>{
+      return response.json();
+  }).then(json=>{
+
+     const part = json.data;
+     const equip1 = part[0].nombre;
+     const equip2 = part[1].nombre;
+     const equip3 = part[2].nombre;
+     const equip4 = part[3].nombre;
+     const equip5 = part[4].nombre;
+     const equip6 = part[5].nombre;
+     const equip7 = part[6].nombre;
+     const equip8 = part[7].nombre;
+
+     partido1.innerHTML = `${equip1} vs ${equip2}`;
+     partido2.innerHTML = `${equip3} vs ${equip4}`;
+     partido3.innerHTML = `${equip5} vs ${equip6}`;
+     partido4.innerHTML = `${equip7} vs ${equip8}`;
+  })
+
+}
+
+setInterval(partido,1000);
