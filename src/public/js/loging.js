41d3d03e48ebda65df18624aa12dd09608980c25
pachperdev <pachperdev@gmail.com>
2022-06-08 16:03:30 -0500

    loging fix

---
+++ src/public/js/loging.js
@@ -0,0 +1,12 @@
+function ir(){
+    var c = 1234;
+    var u = "admin";
+
+    const user = document.getElementById("username").value;
+    const pass = document.getElementById("password").value;
+
+    if (user == u & pass == c) {
+        window.location="minuto-minuto.html"
+    }
+
+}
