1ec48f2a0f541de8522b6c69049865ae7d82a85b
JivaJR <ajavierr@uninorte.edu.co>
2022-06-10 02:31:09 -0500

    cambios javi

--- src/public/js/programacion.js
+++ src/public/js/programacion.js
@@ -1,47 +1,12 @@
-const btn = document.getElementById("btn");
-let email
-let psw
-let rpsw
-let reg
+async function enviar(){
 
-document.getElementById("email").addEventListener("change", function () {
-  email = this.value;
-  console.log(email)
-
-})
-document.getElementById("psw").addEventListener("change", function () {
-  psw = this.value;
-  console.log(psw)
-
-})
-
-
-
-btn.addEventListener("click", function () {
-
-
-
-  reg = [email, psw];
-  console.log(reg)
-
-
-  registro();
-
-});
-
-async function registro() {
-  let response = await fetch(`/registro?email=${email}&psw=${psw}`)
+  let user = document.getElementById("username").value;
+  let pass = document.getElementById("password").value;
+  
+  let response = await fetch(`/login?user=${user}&pass=${pass}`)
   let json = await response.json();
 
-
-
-  if (json.data = !email) {
-
-    alert("Usuario Ingresado");
-
-
-  } else if (json.data == email) {
-    alert("El Usuario que trata de ingresar ya se encuentra registrado")
-  }
+  console.log(json.data[0].email)
+  console.log(json.data[0].contrasena)
 
 }
